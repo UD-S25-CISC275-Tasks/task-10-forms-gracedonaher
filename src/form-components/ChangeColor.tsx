53a5d8a3527c29904f7ca75e0ef7529a341ac834
gracedonaher <gdonaher@udel.edu>
2025-03-19 18:49:25 -0400

    fixing lint error

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap"; // Import Form from react-bootstrap
 
 export function ChangeColor(): React.JSX.Element {
     // state / model
     const [color, setColor] = useState<string>("");
 
     // control
-    function updateColor(color: React.ChangeColor<HTMLInputElement>) {
-        setColor(color.target.value);
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
     }
 
     // view
@@ -16,77 +16,84 @@ export function ChangeColor(): React.JSX.Element {
             <Form.Check
                 type="radio"
                 name="red"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-red"
                 label="red"
-                value="red"
                 data-testid="red-box"
+                value="red"
                 checked={color === "red"}
-            />;
+            />
             <Form.Check
                 type="radio"
                 name="blue"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-blue"
                 label="blue"
                 data-testid="blue-box"
+                value="blue"
                 checked={color === "blue"}
             />
             <Form.Check
                 type="radio"
                 name="green"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-green"
                 label="green"
                 data-testid="green-box"
+                value="green"
                 checked={color === "green"}
             />
             <Form.Check
                 type="radio"
                 name="purple"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-purple"
                 label="purple"
                 data-testid="purple-box"
+                value="purple"
                 checked={color === "purple"}
             />
             <Form.Check
                 type="radio"
                 name="yellow"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-yellow"
                 label="yellow"
                 data-testid="yellow-box"
+                value="yellow"
                 checked={color === "yellow"}
             />
             <Form.Check
                 type="radio"
                 name="pink"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-pink"
                 label="pink"
                 data-testid="pink-box"
+                value="pink"
                 checked={color === "pink"}
             />
             <Form.Check
                 type="radio"
                 name="orange"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-orange"
                 label="orange"
                 data-testid="orange-box"
+                value="orange"
                 checked={color === "orange"}
             />
             <Form.Check
                 type="radio"
                 name="black"
-                onchange={updateColor}
+                onChange={updateColor} // Changed "onchange" to "onChange"
                 id="color-black"
                 label="black"
                 data-testid="black-box"
+                value="black"
                 checked={color === "black"}
             />
             <div>You have chosen "{color}".</div>
-        </div>;
+        </div>
     );
 }
